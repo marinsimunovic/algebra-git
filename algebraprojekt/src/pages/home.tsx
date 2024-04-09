adcce0f70984794c0a8ac5fe63ed77accd17da22
marinsimunovic <3simunovic.marin@gmail.com>
2024-04-09 17:55:44 +0200

    home-page

--- algebraprojekt/src/pages/home.tsx
+++ algebraprojekt/src/pages/home.tsx
@@ -1,91 +1,121 @@
 import { useState } from "react";
-import ChatImg from "../assets/chat.svg";
+import Button from "../components/button";
 import GalleryImg from "../assets/gallery.svg";
 import FollowImg from "../assets/follow.svg";
-import Slider from "../components/slider"; // Import Slider component
-import Button from "../components/button";
+import Guitar from "../assets/hobbies/guitar.svg";
+import Book from "../assets/hobbies/book.svg";
+import Camping from "../assets/hobbies/camping.svg";
+import Bars from "../assets/bars.svg";
+import Spotify from "../assets/spotify.svg";
 type MyInfo = {
   url: string;
   name: string;
   surename: string;
   email: string;
-  hobbies: string[];
-  awards: string;
+  hobbies: {
+    [name: string]: string;
+  };
   insta: string;
   spotify: string;
   facebook: string;
   location: string;
+  birth: string;
+  phone: string;
 };
 
 const Info: MyInfo = {
-  url: "https://instagram.fzag4-1.fna.fbcdn.net/v/t51.2885-19/382536270_796768882457281_2666743802291451333_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fzag4-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=FpbZrDOoVdcAb5JvGwo&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBk17ygKaVJbAGg6_qiOwVXnkZPeO2caoqg6h_kdc3rTw&oe=661A1D66&_nc_sid=8b3546",
+  url: "https://www.toonsmag.com/wp-content/uploads/2023/04/Bart-Simpson-cartoon-686x1024.png",
   name: "Marin",
   surename: "Šimunović",
   email: "3simunovic.marin@gmail.com",
-  hobbies: [
-    "guitar playing: Warm-up with scales (10 mins), practice chord changes (10 mins), work on a new technique or song (15 mins), focus on rhythm exercises (10 mins), end with cool-down improvisation (5 mins).",
-    "reading: Warm-up with sight-reading exercises (10 mins), practice reading rhythms (10 mins), work on a new piece or section (15 mins), focus on dynamics and expression (10 mins), end with sight-reading a new piece (5 mins).",
-    "hiking: Warm-up with stretches (5 mins), start with a moderate incline (30 mins), increase pace on flat terrain (20 mins), enjoy a scenic view or rest (10 mins), descend with focus on balance (25 mins), cool-down stretches (10 mins).",
-  ],
-  awards: "none",
+  hobbies: {
+    guitar:
+      "Creative Expression: The guitar offers a vast range of tones, textures, and techniques, allowing players to express themselves creatively through music. Whether composing original pieces or interpreting existing songs, the guitar provides a platform for artistic expression and personal storytelling. Emotional Outlet: Playing the guitar can be a cathartic experience, allowing musicians to channel their emotions into their music. Whether experiencing joy, sadness, love, or anger, the guitar provides a means to express and process complex feelings, serving as a therapeutic outlet for emotional release. Connection with Others: Music has a unique ability to bring people together, and playing the guitar can foster connections with fellow musicians and audiences alike. Whether jamming with friends, performing live on stage, or simply sharing songs with loved ones, playing the guitar facilitates meaningful connections and shared experiences. Personal Growth: Learning to play the guitar requires dedication, discipline, and perseverance. As players overcome challenges, master new techniques, and expand their musical repertoire, they experience personal growth and a sense of achievement. The journey of learning and improvement fosters self-confidence, resilience, and a lifelong love of learning. Escape and Relaxation: Immersing oneself in music can provide a welcome escape from the stresses and pressures of daily life. Whether strumming soothing chords or shredding energetic riffs, playing the guitar offers a form of relaxation and stress relief, transporting players to a state of flow and ,mindfulness.",
+    reading:
+      "reading:I love books because they are portals to infinite worlds, inviting me to embark on countless adventures without ever leaving my cozy corner. Within their pages, I discover characters who become friends, mentors, or even adversaries, each teaching me something new about the human experience. Books ignite my imagination, transporting me to distant galaxies, historical epochs, or the depths of someone's mind. They offer solace during times of loneliness, enlightenment in moments of confusion, and inspiration when my spirit needs lifting. Through books, I explore different cultures, philosophies, and perspectives, broadening my understanding of the world. The smell of aged paper and the tactile sensation of turning each page evoke a sense of nostalgia and comfort. With each book I read, I evolve, gaining insights and empathy that shape my identity. In the vast universe of literature, I find both refuge and endless possibilities, making my love for books boundless.   Moreover, books serve as timeless repositories of knowledge, capturing the wisdom of generations past and present. They are invaluable sources of information, offering me the opportunity to delve into any subject that piques my curiosity, from science and history to philosophy and art. With each turn of the page, I uncover new perspectives, challenge my assumptions, and expand my intellectual horizons. Whether I seek practical advice, profound insights, or simply a momentary escape, books are steadfast companions, ready to enlighten and enrich my life. In a world often marked by fleeting trends and superficial distractions, the enduring allure of books reminds me of the enduring power of words to inspire, educate, and transform.",
+    camping:
+      "I love camping because it reconnects me with the raw beauty of nature, offering a much-needed escape from the hustle and bustle of modern life. Amidst towering trees, babbling streams, and star-studded skies, I find solace and serenity. The simplicity of life outdoors strips away the distractions of technology, allowing me to immerse myself fully in the present moment. Each camping trip brings a sense of adventure, as I explore new trails, pitch tents under the open sky, and gather around crackling campfires with friends and family.Camping rejuvenates my spirit, revitalizing me with the fresh air and invigorating landscapes. Whether it's the crisp scent of pine needles, the gentle rustle of leaves in the breeze, or the symphony of nocturnal sounds, nature's sensory delights awaken my senses and instill a profound sense of awe. Far from the noise of the city, I find clarity and perspective, reflecting on life's simple pleasures and reconnecting with what truly matters.Moreover, camping fosters a deep appreciation for sustainability and environmental stewardship. As I tread lightly on the earth, practicing Leave No Trace principles and minimizing my ecological footprint, I develop a greater reverence for the delicate balance of ecosystems and the importance of preserving wilderness areas for future generations.But perhaps most importantly, camping fosters bonds of camaraderie and shared experiences. Whether swapping stories under the stars, cooking meals over a camp stove, or embarking on group hikes, the shared challenges and triumphs of outdoor adventures forge lasting memories and strengthen relationships. In the embrace of nature's grandeur, I find not only solitude but also a profound sense of connection – to the earth, to my loved ones, and to the essence of being alive.",
+  },
   insta: "marinsimunovic2",
   spotify: "msimunovic",
   facebook: "Marin Šimunović",
-  location: "Čepin, Croatia",
+  location: "Croatia",
+  birth: "21.02.2002.",
+  phone: "+0915099982",
 };
 
 const Home = () => {
   const [data] = useState<MyInfo>(Info);
+  const [selectedHobby, setSelectedHobby] = useState<string>("guitar");
+
+  const handleHobbyClick = (hobby: string) => {
+    setSelectedHobby(hobby);
+  };
 
   return (
     <>
       <div className="user-profile">
-        <div className="user-links">
-          <div className="user-actions">
+        <div className="user-profile-top">
+          <div className="user-links">
             <Button>
-              <img className="user-img" src={ChatImg} alt="" />
+              <a href="/vjezbe">
+                <img className="bars" src={Bars} alt="" />
+              </a>
             </Button>
+            <div className="user-actions">
+              <Button>
+                <a href="https://www.tiktok.com/@skunk.bend">
+                  <img className="user-img" src={Spotify} alt="" />
+                </a>
+              </Button>
 
-            <Button>
-              <img className="user-img" src={GalleryImg} alt="" />
-            </Button>
+              <Button>
+                <a href="https://www.instagram.com/marinsimunovic2/">
+                  <img className="user-img" src={GalleryImg} alt="" />{" "}
+                </a>
+              </Button>
 
-            <Button>
-              <img className="user-img" src={FollowImg} alt="" />
-            </Button>
+              <Button>
+                <a href="https://m.facebook.com/people/Skunk-bend/100084102875642/">
+                  <img className="user-img" src={FollowImg} alt="" />{" "}
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
-        <div className="user-info">
-          {" "}
-          <img className="user-image" src={data.url} alt="img" />
-          <div className="username">
-            {data.name} {data.surename}
-          </div>
-          <div className="location">{data.location}</div>
-        </div>
-        <div className="user-cards">
-          {" "}
-          <div className="user-hobbies">
-            <h3>My routines</h3>
-            {data.hobbies.map((hobby, index) => (
-              <div className="hobbies" key={index}>
-                <li>{hobby}</li>
+        <div className="user">
+          <div className="user-info">
+            <img className="user-image" src={data.url} alt="img" />
+            <div className="user-data">
+              <div className="username">
+                {data.name} {data.surename}
               </div>
-            ))}
+              <div className="location">{data.location}</div>
+              <div className="phone">{data.phone}</div>
+              <div className="birthday">{data.birth}</div>
+            </div>
           </div>
-          <div className="user-details">
-            <div className="user-details-value-top">{data.email}</div>
-
-            <div className="user-details-value">{data.insta}</div>
-
-            <div className="user-details-value-top">{data.facebook}</div>
-
-            <div className="user-details-value">{data.spotify}</div>
+          <div className="user-cards">
+            <div className="user-hobbies">
+              <div className="user-hobbies-box">
+                <h2>My Hobbies</h2>
+                <div className="hobbies-buttons">
+                  <Button onClick={() => handleHobbyClick("guitar")}>
+                    <img className="hobbies-img" src={Guitar} alt="" />
+                  </Button>
+                  <Button onClick={() => handleHobbyClick("reading")}>
+                    <img className="hobbies-img" src={Book} alt="" />
+                  </Button>
+                  <Button onClick={() => handleHobbyClick("camping")}>
+                    <img className="hobbies-img" src={Camping} alt="" />
+                  </Button>
+                </div>
+              </div>
+              <div className="selected-hobby-details">
+                {selectedHobby && <p>{data.hobbies[selectedHobby]}</p>}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="user-languages">
-          languages: <div className="user-languages-english">English</div>
-          <Slider />
         </div>
       </div>
     </>
--- algebraprojekt/src/pages/vjezbe.tsx
+++ algebraprojekt/src/pages/vjezbe.tsx
@@ -3,15 +3,17 @@ import { vjezbeLinks } from "../data/vjezbe";
 const Vjezbe = () => {
   return (
     <>
-      <h1>Popis vježbi</h1>
-      <div>
-        {vjezbeLinks.map((link) => {
-          return (
-            <a className="link" key={link.href} href={link.href}>
-              {link.label}
-            </a>
-          );
-        })}
+      <div className="Vjezbe-layout">
+        <h1>Popis vježbi</h1>
+        <div>
+          {vjezbeLinks.map((link) => {
+            return (
+              <a className="link" key={link.href} href={link.href}>
+                {link.label}
+              </a>
+            );
+          })}
+        </div>
       </div>
     </>
   );
