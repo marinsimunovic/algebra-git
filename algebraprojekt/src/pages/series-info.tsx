7b050734b0317e94c24f5f9d934c7d77145e3a15
marins <3simunovic.marin@gmail.com>
2024-04-15 21:19:11 +0200

    commit

--- algebraprojekt/src/pages/series-info.tsx
+++ algebraprojekt/src/pages/series-info.tsx
@@ -1,5 +1,35 @@
+import { useParams } from "react-router-dom";
+import { SeriesType, seriesData } from "../data/series";
+import { useEffect, useState } from "react";
+import Arrow from "../assets/arrow.svg";
 const GameInfo = () => {
-  return <>ser</>;
+  const { showId } = useParams();
+  const [data, setData] = useState<SeriesType>();
+  const getShowInfo = () => {
+    const show = seriesData.find((show) => {
+      return show.id === showId;
+    });
+    setData(show);
+  };
+  useEffect(() => {
+    getShowInfo();
+  }, []);
+  return (
+    <>
+      {" "}
+      {data ? (
+        <>
+          <a href="/games">
+            <img className="back" src={Arrow} alt="arrow" />
+          </a>
+        </>
+      ) : (
+        <div>
+          <b>{showId}</b>
+          {"  "}nepostoji
+        </div>
+      )}
+    </>
+  );
 };
-
 export default GameInfo;
